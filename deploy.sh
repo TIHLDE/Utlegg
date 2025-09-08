099138d0946cf2d10d9786704c9277082663b1c5
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-09-08 13:31:36 +0200

    removed prod cache and added commt hash to tag

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+COMMIT_HASH=$(git rev-parse --short HEAD)
+
 echo "-> Building new Docker image"
-docker build -t utlegg.tihlde.org .
+docker build --no-cache -t utlegg.tihlde.org:$COMMIT_HASH .
 
 echo "-> Stopping and removing old container"
 docker rm -f utlegg.tihlde.org || true
 
 echo "-> Starting new container"
-docker run --env-file .env -p 2000:3000 --name utlegg.tihlde.org --restart unless-stopped -d utlegg.tihlde.org
+docker run --env-file .env -p 2000:3000 --name utlegg.tihlde.org --restart unless-stopped -d utlegg.tihlde.org:$COMMIT_HASH
 
