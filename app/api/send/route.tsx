1693d16d6f7f7801bbf70bf85bc4dc1d3a067afd
Mathias Strøm <mathias.strom03@gmail.com>
2025-11-11 11:00:47 +0100

    Switched to finansminister

--- app/api/send/route.tsx
+++ app/api/send/route.tsx
@@ -128,7 +128,7 @@ export async function POST(req: Request) {
     });
 
     const financeRecipients = [
-      "teknologiminister@tihlde.org",
+      "finansminister@tihlde.org",
       ...(ccEmail ? [ccEmail] : []),
     ];
 
